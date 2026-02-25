a50c2cfc3498a3239e22bf66317240dc2ec2abc0
chiefkhanman <farhanaadit@gmail.com>
2026-02-25 09:39:43 -0600

    added comments for future tasks

--- logger.py
+++ logger.py
@@ -5,15 +5,26 @@ class Logger:
     def log(self,):
         pass
 
-    def log_step(self,):
+    def log_step(self, *args, **kwargs):
+        # the airspace state will be used for plotting the vertiports
+        # the atc_state will be used for UAV positions for plotting 
         pass
 
     def get_simulator_start_metrics(self,):
+        # use database to calculate the start metrics 
+        # the database will be used to fetch data needed for calculating the start metrics
         pass
 
     def get_simulator_end_metrics(self,):
+        # use database to calculate the end metrics 
+        # the database will be used to fetch data needed for calculating the end metrics
         pass
 
 
     def reset(self,):
+        # create new database to store states
+        pass
+
+    def get_step_render_data(self,):
+        # for each step of the simulator get the data needed for rendering that step
         pass
