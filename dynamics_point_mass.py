447e4a9f5d325b052dbb4931788a03c11fd33646
chiefkhanman <farhanaadit@gmail.com>
2026-02-27 19:45:20 -0600

    updated PointMassDynamics - corrected implementation for Non-holonomic dynamics

--- dynamics_point_mass.py
+++ dynamics_point_mass.py
@@ -1,19 +1,28 @@
 import math
+import numpy as np
+from shapely import Point
 from dynamics_template import Dynamics
 from uav import UAV
 from uav_template import UAV_template
 
 class PointMass(Dynamics):
+    ''' Non holonomic model '''
     def __init__(self,):
         super().__init__()
 
 
     def step(self, action, uav: UAV|UAV_template) -> None:
         #! make sure heading_change is in radians
-        acceleration, heading_change = action
-
+        acceleration, yaw_rate_command = action
+        acceleration = np.clip(acceleration, -uav.max_acceleration, uav.max_acceleration)
         # update heading
-        uav.current_heading += heading_change
+        if uav.current_speed > 0.1:
+            dynamic_max_yaw_rate = uav.max_lateral_acceleration/uav.current_speed
+            yaw_rate = np.clip(yaw_rate_command, -dynamic_max_yaw_rate, dynamic_max_yaw_rate)
+        else:
+            yaw_rate = yaw_rate_command
+
+        uav.current_heading += yaw_rate * self.dt
         # Wrap heading to [-π, π].
         # (θ + π) % (2π) − π maps any angle onto [-π, π]:
         #   shift up by π  →  [0, 2π] domain for modulo
@@ -27,3 +36,4 @@ class PointMass(Dynamics):
         uav.vx = uav.current_speed * math.cos(uav.current_heading)
         uav.vy = uav.current_speed * math.sin(uav.current_heading)
         # uav.vz -> unchanged
+        uav.current_position = Point(uav.current_position.x + uav.vx * self.dt, uav.current_position.y + uav.vy * self.dt)
